80d017656d4039c73452eb53bbf84a58701f4b35
Somsubhra Bairi <somsubhra.bairi@gmail.com>
2014-03-31 12:55:44 +0530

    Added comments

--- src/Parser.py
+++ src/Parser.py
@@ -1,8 +1,14 @@
 class Parser:
     def __init__(self, filename):
+
         self.filename = filename
+
+        # Read the contents of file as UTF-8
         self.contents = open(filename).read()
         self.contents = self.contents.decode("utf-8")
 
+        # Find the words in content
         self.words = self.contents.split()
+
+        # Find the number of words in the content
         self.number_of_words = len(self.words)
