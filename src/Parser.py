d6ecc83fee3a1a05539a17e41a5f8688e956013d
Somsubhra Bairi <somsubhra.bairi@gmail.com>
2014-03-31 20:17:11 +0530

    Added script. Refactor.

--- src/Parser.py
+++ src/Parser.py
@@ -9,22 +9,19 @@ class Parser:
         self.contents = open(filename).read()
         self.contents = self.contents.decode("utf-8")
 
-    # The words of the content
-    def words(self):
-        return self.contents.split()
+        self.words = self.contents.split()
 
     # Number of words in the content
     def number_of_words(self):
-        return len(self.words())
+        return len(self.words)
 
     # Number of sentences in the content
     def number_of_sentences(self):
         number = 0
-        words = self.words()
 
         separators = [u".", u"!", u"?"]
 
-        for word in words:
+        for word in self.words:
             if word[-1] in separators and len(word) > 1:
                 number += 1
 
--- src/main.py
+++ src/main.py
@@ -2,9 +2,9 @@ from Parser import *
 
 
 def main():
-    p = Parser('../res/test')
+    p = Parser('res/test')
     print p.number_of_words()
-    print p.words()
+    print p.words
     print p.number_of_sentences()
 
 if __name__ == '__main__':
