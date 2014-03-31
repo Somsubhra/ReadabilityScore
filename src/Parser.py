c69ab5fc49e5fb0c6f2dce744c844b7ba212053e
Somsubhra Bairi <somsubhra.bairi@gmail.com>
2014-03-31 13:16:55 +0530

    Count number of sentences

--- src/Parser.py
+++ src/Parser.py
@@ -1,14 +1,31 @@
+# Parser class
 class Parser:
-    def __init__(self, filename):
 
+    # Constructor for parser class
+    def __init__(self, filename):
         self.filename = filename
 
         # Read the contents of file as UTF-8
         self.contents = open(filename).read()
         self.contents = self.contents.decode("utf-8")
 
-        # Find the words in content
-        self.words = self.contents.split()
+    # The words of the content
+    def words(self):
+        return self.contents.split()
 
-        # Find the number of words in the content
-        self.number_of_words = len(self.words)
+    # Number of words in the content
+    def number_of_words(self):
+        return len(self.words())
+
+    # Number of sentences in the content
+    def number_of_sentences(self):
+        number = 0
+        words = self.words()
+
+        separators = [u".", u"!", u"?"]
+
+        for word in words:
+            if word[-1] in separators and len(word) > 1:
+                number += 1
+
+        return number
--- src/main.py
+++ src/main.py
@@ -3,8 +3,9 @@ from Parser import *
 
 def main():
     p = Parser('../res/test')
-    print p.number_of_words
-    print p.words
+    print p.number_of_words()
+    print p.words()
+    print p.number_of_sentences()
 
 if __name__ == '__main__':
     main()
