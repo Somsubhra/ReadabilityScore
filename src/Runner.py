8e945d2cfc231e277a54002721d1e51555061ac3
Somsubhra Bairi <somsubhra.bairi@gmail.com>
2014-04-01 01:06:24 +0530

    A complete runner

--- src/Runner.py
+++ src/Runner.py
@@ -12,12 +12,33 @@ class Runner:
 
     # The main running method for the runner
     def run(self):
-        f = []
+
+        # Create output directory
+        try:
+            stat(self.output_directory)
+        except:
+            mkdir(self.output_directory)
+
+        # Write to output file
+        output_file = open('out/result.csv', 'w+')
+
+        output_file.write("\"Filename\";\"Number of words\";\"Number of sentences\";"
+                          "\"Avg. words per sentence\";\"Avg. word length\";\"Number of Jukthakshars\"\n")
+
+        # Walk through all files in tree
         for (dir_path, _, file_names) in walk(self.input_directory):
             for filename in file_names:
                 test_file = path.join(dir_path, filename)
+
+                # Calculations
                 p = Parser(test_file)
-                try:
-                    stat(self.output_directory)
-                except:
-                    mkdir(self.output_directory)
+
+                # The output line
+                output_line = "\"" + str(test_file) + "\";\"" + str(p.number_of_words()) + "\";\"" +\
+                              str(p.number_of_sentences()) + "\";\"" + str(p.average_words_per_sentence()) \
+                              + "\";\"" + str(p.average_word_length()) + "\";\"" + str(p.number_of_jukthakshar()) +\
+                              "\"\n"
+
+                output_file.write(output_line)
+
+        output_file.close()
