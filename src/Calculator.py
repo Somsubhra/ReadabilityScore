feed587627d056e283140e87ba3d4c61f4997dc8
Somsubhra Bairi <somsubhra.bairi@gmail.com>
2014-04-01 23:13:21 +0530

    Added Flesch Kincaid grade level

--- src/Calculator.py
+++ src/Calculator.py
@@ -27,3 +27,7 @@ class Calculator:
     def flesch_reading_ease(self):
         return 206.835 - 1.105 * (float(self.parser.number_of_words()) / float(self.parser.number_of_sentences())) - \
                84.6 * (float(self.parser.number_of_syllables()) / float(self.parser.number_of_words()))
+
+    # Calculate Flesch Kincaid grade level
+    def flesch_kincaid_frade_level(self):
+        return (0.39 * float(self.parser.average_words_per_sentence())) + (11.8 * self.parser.average_syllable_per_word()) - 15.59
--- src/Runner.py
+++ src/Runner.py
@@ -38,7 +38,7 @@ class Runner:
                 # The parser
                 p = c.parser
 
-                print c.flesch_reading_ease()
+                print c.flesch_kincaid_frade_level()
 
                 # The output line
                 output_line = "\"" + str(test_file) + "\";\"" + str(p.number_of_words()) + "\";\"" +\
